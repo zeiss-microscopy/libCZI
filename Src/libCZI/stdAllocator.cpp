db630c38d31470a4bfe7d823d31025194c10d0de
Simon Harris <haruki_zaemon@mac.com>
2018-01-25 09:20:18 +1100

    Compiles on macOS
    Apple doesn’t support aligned_alloc. That said, allocation is already aligned so it works out fine anyway.

--- Src/libCZI/stdAllocator.cpp
+++ Src/libCZI/stdAllocator.cpp
@@ -1,23 +1,23 @@
 //******************************************************************************
-// 
+//
 // libCZI is a reader for the CZI fileformat written in C++
 // Copyright (C) 2017  Zeiss Microscopy GmbH
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 // To obtain a commercial version please contact Zeiss Microscopy GmbH.
-// 
+//
 //******************************************************************************
 
 #include "stdafx.h"
@@ -30,7 +30,7 @@ void* CHeapAllocator::Allocate(std::uint64_t size)
 	{
 		throw std::out_of_range("The requested size for allocation is out-of-range.");
 	}
-#if defined(__EMSCRIPTEN__)
+#if defined(__EMSCRIPTEN__)||defined(__APPLE__)
 	return malloc((size_t)size);
 #else
 #if defined(__GNUC__)
