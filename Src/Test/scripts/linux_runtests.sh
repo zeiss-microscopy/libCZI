100bb9f714a7b9c1342be77e5e90ddc0188b775f
Unknown <jbohl@h-quer.de>
2018-02-01 00:02:31 +0100

    modify paths

--- Src/Test/scripts/linux_runtests.sh
+++ Src/Test/scripts/linux_runtests.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-#EXECUTABLE=~/Dev/BuildNaCZIr/CZICmd/CZIcmd
-EXECUTABLE=~/Dev/NaCZIr/CZICmd/CZIcmd
-SRCIMAGES=/datadrive/NaCZIr/TestData/
+#EXECUTABLE=~/github/libCZI/Src/CZIcmd/CZIcmd
+EXECUTABLE=~/github/libCZI/Src/CZICmd/CZIcmd
+SRCIMAGES=/home/jbohl/atom2/hdd/2/tmp/TestData/
 
 if [ -z "$1"] 
 then
-  python runtests.py -s $SRCIMAGES -d /home/jbohl/Data/out/ -e $EXECUTABLE -t ./testdata_linux.txt 
+  python runtests.py -s $SRCIMAGES -d /home/jbohl/atom2/hdd/2/tmp/TestOut/ -e $EXECUTABLE -t ./testdata_linux.txt 
 else
-  python runtests.py -s $SRCIMAGES -d /home/jbohl/Data/out/ -e $EXECUTABLE -t ./testdata_linux.txt --skip $1
+  python runtests.py -s $SRCIMAGES -d /home/jbohl/atom2/hdd/2/tmp/TestOut/ -e $EXECUTABLE -t ./testdata_linux.txt --skip $1
 fi
 
